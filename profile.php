f93a43191d6986eba6b5b1320a8cb8c93937cc36
sirkdev98 <vicentekhristian@gmail.com>
2022-06-15 00:23:45 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -373,6 +373,7 @@ if(isset($_SESSION['username'])){
                             $mvfileno = $row['mvfileno']; 
                             $plateno = $row['plateno']; 
                             $chasisno = $row['chasisno']; 
+                            $engineno = $row['engineno']; 
                             $maker = $row['maker']; 
                             $pistondisp = $row['pistondisp']; 
                             $cor = $row['cor']; 
@@ -444,45 +445,42 @@ if(isset($_SESSION['username'])){
               <div class="tab-content pt-2">
 
                 <div class="tab-pane fade show active profile-overview" id="profile-overview">
-                  <h5 class="card-title">About</h5>
-                  <p class="small fst-italic">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</p>
+                  <h5 class="card-title">Tricycle Details</h5>
+                  <p class="small fst-italic">Below are the details for the selected tricycle</p>
 
-                  <h5 class="card-title">Profile Details</h5>
+                  <h5 class="card-title"><?php echo $toda; ?> - <?php echo $bodynum; ?></h5>
 
                   <div class="row">
-                    <div class="col-lg-3 col-md-4 label ">Full Name</div>
-                    <div class="col-lg-9 col-md-8">Kevin Anderson</div>
+                    <div class="col-lg-3 col-md-4 label ">MV File No.</div>
+                    <div class="col-lg-9 col-md-8"><?php echo $mvfileno; ?></div>
                   </div>
 
                   <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Company</div>
-                    <div class="col-lg-9 col-md-8">Lueilwitz, Wisoky and Leuschke</div>
+                    <div class="col-lg-3 col-md-4 label">Plate Number</div>
+                    <div class="col-lg-9 col-md-8"><?php echo $plateno; ?></div>
                   </div>
 
                   <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Job</div>
-                    <div class="col-lg-9 col-md-8">Web Designer</div>
+                    <div class="col-lg-3 col-md-4 label">Engine Number</div>
+                    <div class="col-lg-9 col-md-8"><?php echo $engineno; ?></div>
                   </div>
 
                   <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Country</div>
-                    <div class="col-lg-9 col-md-8">USA</div>
+                    <div class="col-lg-3 col-md-4 label">Chasis Number</div>
+                    <div class="col-lg-9 col-md-8"><?php echo $chasisno; ?></div>
                   </div>
 
                   <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Address</div>
-                    <div class="col-lg-9 col-md-8">A108 Adam Street, New York, NY 535022</div>
+                    <div class="col-lg-3 col-md-4 label">Sidecar Color</div>
+                    <div class="col-lg-9 col-md-8"><font color="<?php echo $sidecarcolor; ?>"><?php echo $sidecarcolor; ?></font></div>
                   </div>
 
                   <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Phone</div>
-                    <div class="col-lg-9 col-md-8">(436) 486-3538 x29071</div>
+                    <div class="col-lg-3 col-md-4 label">Franchise Date</div>
+                    <div class="col-lg-9 col-md-8"><?php echo $franchisedate; ?></div>
                   </div>
 
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Email</div>
-                    <div class="col-lg-9 col-md-8">k.anderson@example.com</div>
-                  </div>
+                 
 
                 </div>
 
