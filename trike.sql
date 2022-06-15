1f4c434c85a6477874b4188a32ef2bce61f497d9
sirkdev98 <vicentekhristian@gmail.com>
2022-06-15 20:30:03 +0800

    Update trike.sql

--- trike.sql
+++ trike.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2022 at 02:43 AM
+-- Generation Time: Jun 15, 2022 at 02:29 PM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.6.14
 
@@ -64,7 +64,47 @@ INSERT INTO `driveroperator` (`pid`, `fname`, `mname`, `lname`, `extname`, `addr
 (11, 'OLIVER', 'MATAWARAN', 'LALUNA', '', 'pant@asd.com', 'Balut', '', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-14', 'OPERATOR/DRIVER', '123PCSS', 'OPERATOR', '2022-06-21', '62a839d09d403.png'),
 (12, 'OLIVER', 'MATAWARAN', 'LALUNA', '', 'pant@asd.com', 'Balut', '', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-14', 'OPERATOR/DRIVER', '123PCSS', 'OPERATOR', '2022-06-21', '62a83aae57670.png'),
 (13, 'Testing fname', 'Middle', 'Last', 'ext', 'Leona@asd.acom', '09275109013', 'Apollo', 'Testing fname', 'Testing fname', 'Testing fname', '2022-06-14', 'OPERATOR/DRIVER', '22112', 'OPERATOR', '2022-06-14', '62a8a8272cfac.png'),
-(14, 'test2', 'test2', 'test2', 'test2', '22211@asd.com', '09275109013', 'Calero', 'Testing fname', 'Testing fname', 'Testing fname', '2022-06-14', 'OPERATOR/DRIVER', '22112', 'OPERATOR', '2022-06-15', '62a8a8e6ce47a.png');
+(14, 'test2', 'test2', 'test2', 'test2', '22211@asd.com', '09275109013', 'Calero', 'Testing fname', 'Testing fname', 'Testing fname', '2022-06-14', 'OPERATOR/DRIVER', '22112', 'OPERATOR', '2022-06-15', '62a8a8e6ce47a.png'),
+(15, 'OLIVERxxxxxxx', 'MATAWARANxxx', 'LALUNAxx', '123123', '', '', 'Balut', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-16', '', '123asd', 'OPERATOR/DRIVER', '', '62a98629823d5.png'),
+(16, 'SUBOK', 'SUBOK', 'SUBOK', 'SUBOK', '', '', 'Apollo', 'NAME', 'OF', 'SPOUSE', '2022-06-22', '', '123asd', 'OPERATOR', '', '62a9869b5c963.png'),
+(17, 'SUBOK', 'SUBOK', 'SUBOK', 'SUBOK', '', 'Apollo', '', 'NAME', 'OF', 'SPOUSE', '2022-06-22', '', '123asd', 'OPERATOR', '', '62a98751a9f63.png'),
+(18, 'testadd', 'testtestest', 'testadddd', 'qwe', 'add@gawsd.com', 'Apollo', '09082646040', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-15', '', '1233321', 'OPERATOR', '', '62a987ff37faf.png'),
+(19, 'testadd', 'testtestest', 'testadddd', 'qwe', 'add@gawsd.com', 'Apollo', '09082646040', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-15', '', '1233321', 'OPERATOR', '', '62a9883d4f830.png'),
+(20, 'sir', 'k', 'l', 'ex', 'add@asd.com', 'Apollo', '090909090', 'khrisss', 'tiaan', 'vvvv', '2022-06-16', 'OPERATOR', 'liccc', '', '2022-06-15', '62a98873a6f38.png'),
+(21, 'sir', 'k', 'l', 'ex', 'add@asd.com', 'Apollo', '090909090', 'khrisss', 'tiaan', 'vvvv', '2022-06-16', 'OPERATOR', 'liccc', 'OPERATOR', '2022-06-15', '62a98886dec55.png'),
+(22, 'sir', 'k', 'l', 'ex', 'add@asd.com', 'Apollo', '090909090', 'khrisss', 'tiaan', 'vvvv', '2022-06-16', 'OPERATOR', 'liccc', '', '2022-06-15', '62a988d40f859.png'),
+(23, 'aaaaOLIVER', 'aaaaaaaaaaMATAWARAN', 'aaaaaaaaLALUNA', 'a', 'ASDA@ASDA.COM', 'Apollo', '09082646040', 'tesst1', 'tesst2', 'tesst3', '2022-06-16', '', '123asd', 'OPERATOR', '2022-06-15', '62a98905bc766.png'),
+(24, 'aaaaOLIVER', 'aaaaaaaaaaMATAWARAN', 'aaaaaaaaLALUNA', 'a', 'ASDA@ASDA.COM', 'Apollo', '09082646040', 'tesst1', 'tesst2', 'tesst3', '2022-06-16', '', '123asd', 'OPERATOR', '2022-06-15', '62a98a12a3195.png'),
+(25, 'aaaaOLIVER', 'aaaaaaaaaaMATAWARAN', 'aaaaaaaaLALUNA', 'a', 'ASDA@ASDA.COM', 'Apollo', '09082646040', 'tesst1', 'tesst2', 'tesst3', '2022-06-16', '', '123asd', 'OPERATOR', '2022-06-15', '62a98a43984cd.png'),
+(26, 'SSSSSS', 'DDDDDDDDD', 'FFFFFFf', 'G', 'address', 'Apollo', '09090909', 'Khristian', 'Harold', 'Vicente', '2022-06-15', 'DRIVER', '123asd', 'OPERATOR', '2022-06-15', '62a98a8b1becf.png'),
+(27, 'SSSSSS', 'DDDDDDDDD', 'FFFFFFf', 'G', 'address', 'Apollo', '09090909', 'Khristian', 'Harold', 'Vicente', '2022-06-15', 'DRIVER', '123asd', 'OPERATOR', '2022-06-15', '62a98c3c4ee18.png'),
+(28, 'Heyy', 'Heyy', 'Heyy', 'Heyy', '092755555', 'Apollo', '09275109013', 'Heyy', 'Heyy', 'Heyy', '2022-06-16', 'DRIVER', '123asd', 'OPERATOR', '2022-06-16', '62a98cc960a9e.png'),
+(29, 'Heyy', 'Heyy', 'Heyy', 'Heyy', '092755555', 'Apollo', '09275109013', 'Heyy', 'Heyy', 'Heyy', '2022-06-16', 'DRIVER', '123asd', 'OPERATOR', '2022-06-16', '62a98dc976467.png'),
+(30, 'Try', 'my ', 'best', 'hehe', 'orani', 'Apollo', '12345678901', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-15', 'DRIVER', '123asd', 'OPERATOR', '2022-06-16', '62a990fcb85e7.png'),
+(31, 'OLIVER', 'MATAWARAN', 'LALUNA', 'jr', 'TALA', 'Apollo', '09082646040', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-29', 'DRIVER', 'liccc', 'OPERATOR', '2022-06-23', '62a9916b1e2dd.png'),
+(32, 'OLIVER', 'MATAWARAN', 'LALUNA', '', 'TALA', 'Balut', '09082646040', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-14', 'DRIVER', '1233321', 'OPERATOR/DRIVER', '2022-06-16', '62a9991060701.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drivers`
+--
+
+CREATE TABLE `drivers` (
+  `id` int(55) NOT NULL,
+  `bdynumber` varchar(55) NOT NULL,
+  `trikeid` varchar(55) NOT NULL,
+  `driverid` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `drivers`
+--
+
+INSERT INTO `drivers` (`id`, `bdynumber`, `trikeid`, `driverid`) VALUES
+(1, '1234', '', '30'),
+(2, '1234', '5', '31'),
+(3, '1234', '5', '32');
 
 -- --------------------------------------------------------
 
@@ -141,6 +181,12 @@ ALTER TABLE `driveroperator`
   ADD PRIMARY KEY (`pid`);
 
 --
+-- Indexes for table `drivers`
+--
+ALTER TABLE `drivers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
@@ -161,7 +207,12 @@ ALTER TABLE `tricycle`
 -- AUTO_INCREMENT for table `driveroperator`
 --
 ALTER TABLE `driveroperator`
-  MODIFY `pid` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `pid` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+--
+-- AUTO_INCREMENT for table `drivers`
+--
+ALTER TABLE `drivers`
+  MODIFY `id` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
