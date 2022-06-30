865de473dbab2d1ce128040165d1881eb70883e3
sirkdev98 <vicentekhristian@gmail.com>
2022-06-30 16:53:41 +0800

    Update trike.sql

--- trike.sql
+++ trike.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2022 at 09:35 AM
+-- Generation Time: Jun 30, 2022 at 10:53 AM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.6.14
 
@@ -19,6 +19,155 @@ SET time_zone = "+00:00";
 --
 -- Database: `trike`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driveroperator`
+--
+
+CREATE TABLE `driveroperator` (
+  `pid` int(150) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `mname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `extname` varchar(55) NOT NULL,
+  `address1` varchar(50) NOT NULL,
+  `barangay` varchar(50) NOT NULL,
+  `contactnumber` varchar(55) NOT NULL,
+  `sfname` varchar(50) NOT NULL,
+  `smname` varchar(50) NOT NULL,
+  `slaname` varchar(50) NOT NULL,
+  `bday` date NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `licensenum` varchar(50) NOT NULL,
+  `licensetype` varchar(50) NOT NULL,
+  `licensevalid` varchar(50) NOT NULL,
+  `picname` varchar(52) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `driveroperator`
+--
+
+INSERT INTO `driveroperator` (`pid`, `fname`, `mname`, `lname`, `gender`, `extname`, `address1`, `barangay`, `contactnumber`, `sfname`, `smname`, `slaname`, `bday`, `type`, `licensenum`, `licensetype`, `licensevalid`, `picname`) VALUES
+(81, 'OLIVER', 'MATAWARAN', 'LALUNA', 'MALE', '', 'TALA', '09082646040', 'Apollo', 'OLIVER', 'MATAWARAN', 'LALUNA', '2022-06-30', 'OPERATOR', '123asd', 'OPERATOR', '2022-06-30', '62bcf0fe4f867.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drivers`
+--
+
+CREATE TABLE `drivers` (
+  `id` int(55) NOT NULL,
+  `bdynumber` varchar(55) NOT NULL,
+  `trikeid` varchar(55) NOT NULL,
+  `driverid` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inspection`
+--
+
+CREATE TABLE `inspection` (
+  `id` int(11) NOT NULL,
+  `sidecar_windshield` varchar(55) NOT NULL,
+  `funcitioning_horn` varchar(50) NOT NULL,
+  `signal_lights` varchar(55) NOT NULL,
+  `tail_lights` varchar(55) NOT NULL,
+  `topchain_cover` varchar(55) NOT NULL,
+  `whitered_headlights` varchar(55) NOT NULL,
+  `light_inside` varchar(55) NOT NULL,
+  `mufflers` varchar(55) NOT NULL,
+  `roof` varchar(55) NOT NULL,
+  `side_mirrors` varchar(55) NOT NULL,
+  `upholstery` varchar(55) NOT NULL,
+  `wheels` varchar(55) NOT NULL,
+  `remarks` varchar(55) NOT NULL,
+  `trikeid` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inspection`
+--
+
+INSERT INTO `inspection` (`id`, `sidecar_windshield`, `funcitioning_horn`, `signal_lights`, `tail_lights`, `topchain_cover`, `whitered_headlights`, `light_inside`, `mufflers`, `roof`, `side_mirrors`, `upholstery`, `wheels`, `remarks`, `trikeid`) VALUES
+(49, '', '', '', '', '', '', '', '', '', '', '', '', '', '3095');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mtop`
+--
+
+CREATE TABLE `mtop` (
+  `id` int(50) NOT NULL,
+  `mtopdate` date NOT NULL,
+  `mtopexpiration` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `mtopyear` varchar(50) NOT NULL,
+  `trikeid` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mtopamount`
+--
+
+CREATE TABLE `mtopamount` (
+  `id` int(55) NOT NULL,
+  `mtopfee` varchar(55) NOT NULL,
+  `annualstickerfee` varchar(55) NOT NULL,
+  `mtopplatefee` varchar(55) NOT NULL,
+  `operatoridfee` varchar(55) NOT NULL,
+  `driveridfee` varchar(55) NOT NULL,
+  `parkingfee` varchar(55) NOT NULL,
+  `droppingfee` varchar(55) NOT NULL,
+  `confirmationfee` varchar(55) NOT NULL,
+  `certificationofnorecordfee` varchar(55) NOT NULL,
+  `transferfee` varchar(55) NOT NULL,
+  `farematrix` varchar(55) NOT NULL,
+  `bodynumberstickerfee` varchar(55) NOT NULL,
+  `environmentalfee` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mtopamount`
+--
+
+INSERT INTO `mtopamount` (`id`, `mtopfee`, `annualstickerfee`, `mtopplatefee`, `operatoridfee`, `driveridfee`, `parkingfee`, `droppingfee`, `confirmationfee`, `certificationofnorecordfee`, `transferfee`, `farematrix`, `bodynumberstickerfee`, `environmentalfee`) VALUES
+(1, '200', '150', '250', '25', '25', '35', '60', '100', '100', '1000', '50', '300', '75');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `middle_name` varchar(32) NOT NULL,
+  `role` varchar(24) NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `mac_address` varchar(32) NOT NULL,
+  `date_reg` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `first_name`, `last_name`, `middle_name`, `role`, `position`, `mac_address`, `date_reg`) VALUES
+(2, 'admin', 'admin1', 'Khristian Harold', 'Vicente', '', 'ADMIN', '', '', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -91,9 +240,100 @@ INSERT INTO `toda` (`id`, `toda`, `barangay`, `description`, `color`) VALUES
 (52, 'WATODA', '', '', 'BLUE'),
 (53, 'ZAMTODA', '', '', 'YELLOW');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(50) NOT NULL,
+  `transaction` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `trikeid` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tricycle`
+--
+
+CREATE TABLE `tricycle` (
+  `id` int(120) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `mname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `extname` varchar(44) NOT NULL,
+  `mvfileno` varchar(50) NOT NULL,
+  `plateno` varchar(50) NOT NULL,
+  `engineno` varchar(50) NOT NULL,
+  `chasisno` varchar(50) NOT NULL,
+  `maker` varchar(50) NOT NULL,
+  `motorcolor` varchar(55) NOT NULL,
+  `pistondisp` varchar(50) NOT NULL,
+  `cor` varchar(50) NOT NULL,
+  `ornum` varchar(50) NOT NULL,
+  `applicationdate` varchar(50) NOT NULL,
+  `brgycode` varchar(22) NOT NULL,
+  `bodynum` varchar(50) NOT NULL,
+  `operatorid` varchar(50) NOT NULL,
+  `toda` varchar(55) NOT NULL,
+  `sidecarcolor` varchar(55) NOT NULL,
+  `inspectionstat` varchar(55) NOT NULL,
+  `classification` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tricycle`
+--
+
+INSERT INTO `tricycle` (`id`, `fname`, `mname`, `lname`, `extname`, `mvfileno`, `plateno`, `engineno`, `chasisno`, `maker`, `motorcolor`, `pistondisp`, `cor`, `ornum`, `applicationdate`, `brgycode`, `bodynum`, `operatorid`, `toda`, `sidecarcolor`, `inspectionstat`, `classification`) VALUES
+(3095, 'MATAWARAN', 'MATAWARAN', 'LALUNA', '', '231', '123123', '333', '1312', 'toyota', 'red', '123123', 'adasd', '90221', '2022-06-30', '66', '2421', '81', 'KAMADO ', 'GREEN', '', 'Public');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `driveroperator`
+--
+ALTER TABLE `driveroperator`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `drivers`
+--
+ALTER TABLE `drivers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inspection`
+--
+ALTER TABLE `inspection`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mtop`
+--
+ALTER TABLE `mtop`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mtopamount`
+--
+ALTER TABLE `mtopamount`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `toda`
@@ -102,14 +342,66 @@ ALTER TABLE `toda`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tricycle`
+--
+ALTER TABLE `tricycle`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `driveroperator`
+--
+ALTER TABLE `driveroperator`
+  MODIFY `pid` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+--
+-- AUTO_INCREMENT for table `drivers`
+--
+ALTER TABLE `drivers`
+  MODIFY `id` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT for table `inspection`
+--
+ALTER TABLE `inspection`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+--
+-- AUTO_INCREMENT for table `mtop`
+--
+ALTER TABLE `mtop`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mtopamount`
+--
+ALTER TABLE `mtopamount`
+  MODIFY `id` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `toda`
 --
 ALTER TABLE `toda`
   MODIFY `id` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+--
+-- AUTO_INCREMENT for table `tricycle`
+--
+ALTER TABLE `tricycle`
+  MODIFY `id` int(120) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3096;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
