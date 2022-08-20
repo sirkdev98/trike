b80f1944dc98b63a870508c4e3f4e60e39e012b1
sirkdev98 <vicentekhristian@gmail.com>
2022-08-20 12:15:56 +0800

    Update trikedata.php

--- trikedata.php
+++ trikedata.php
@@ -759,6 +759,27 @@ if(isset($_SESSION['username'])){
 
 
 <br>
+                     <div class="row mb-6">
+                  <label for="inputEmail" class="col-sm-2 col-form-label"><b>Other Details</b></label>
+                  <div class="col-sm-3">
+                    Parking
+                    <input type="text" class="form-control" name="hasparking" placeholder="Parking location" >
+                  </div>
+                  <div class="col-sm-3">
+                    Acquisition date
+                    <input type="date" class="form-control" name="acquisitiondate" placeholder="Date">
+                  </div>
+                  <div class="col-sm-3">
+                    Year Model
+                    <input type="text" class="form-control" placeholder="Year model" name="yearmodel" maxlength="4"  min="0" max="9999" step="1"pattern="[0-9]{2}">
+                  </div>
+                 
+                  </div>
+
+
+
+
+<br>
 
                      <div class="row mb-6">
                   <label for="inputEmail" class="col-sm-2 col-form-label"><b>FRANCHISE</b></label>
